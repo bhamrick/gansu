dfdf2452f41b44adea864c31cd70b0221b2f7d58
Brian Hamrick <brian.c.hamrick@gmail.com>
2008-04-20 11:43:02 -0400

    Switched PIT code to use 1000Hz and to keep track of hours/minutes/seconds

--- src/pit.asm
+++ src/pit.asm
@@ -5,12 +5,12 @@ global pit_handler
 extern kprintf
 
 init_pit:
-	; Sets the pit to 100 Hz
+	; Sets the pit to 1000 Hz
 	mov al,0x36
 	out 0x43,al
-	mov al,0x9b
+	mov al,0xa9
 	out 0x40,al
-	mov al,0x2e
+	mov al,0x04
 	out 0x40,al
 	ret
 
@@ -18,18 +18,43 @@ pit_handler:
 	cli
 	inc dword [ticks]
 	mov eax,[ticks]
-	mov ebx,100
+	mov ebx,1000
 	xor ecx,ecx
 	xor edx,edx
 	idiv ebx
 	cmp edx,0
 	jne .noprint
+.inc:
+	mov eax,[seconds]
+	mov ebx,[minutes]
+	mov ecx,[hours]
+	inc eax
+	cmp eax,60
+	jl .print
+	xor eax,eax
+	inc ebx
+	cmp ebx,60
+	jl .print
+	xor ebx,ebx
+	inc ecx
+	cmp ecx,24
+	jl .print
+	xor ecx,ecx
 .print:
+	mov [seconds],eax
+	mov [minutes],ebx
+	mov [hours],ecx
+	mov eax,[seconds]
+	push eax
+	mov eax,[minutes]
+	push eax
+	mov eax,[hours]
+	push eax
 	mov eax,[ticks]
 	push eax
 	mov eax,handler_fmt_str
 	call kprintf
-	add esp,4
+	add esp,16
 .noprint:
 	mov al,0x20
 	out 0x20,al
@@ -38,4 +63,7 @@ pit_handler:
 
 section .data
 ticks:	dd 0
-handler_fmt_str: db 'Ticks: %d',10,0
+hours:	dd 0
+minutes:dd 0
+seconds:dd 0
+handler_fmt_str: db 'Ticks: %d (%d:%d:%d)',10,0
